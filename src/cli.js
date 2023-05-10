80327517cfa9db903a9ed94896fe088a75cf455d
Medvedev Ramin <r.medvedev2004@gmail.com>
2023-05-10 12:53:47 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+    console.log('May I have your name?');
+    const name = readlineSync.question('Your answer: ');
+    console.log(`Hello, ${name}!`);
+};
+export default userName;
