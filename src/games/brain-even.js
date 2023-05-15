d09139af9bab19c40b77275df8344e82fbb0786c
Medvedev Ramin <r.medvedev2004@gmail.com>
2023-05-15 11:23:04 +0300

    step 5

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,3 +1,20 @@
+import getRandomNum from '../random.js';
+import rulesOfTheGame from '../rulesOfTheGame.js';
 
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+const rangemin = 1;
+const rangemax = 100;
+const isEven = (num) => num % 2 === 0;
+
+const game = () => {
+  const number = getRandomNum(rangemin, rangemax);
+  const correctanswer = isEven(number) ? 'yes' : 'no';
+  const Quest = number;
+  return [correctanswer, Quest];
+  
+};
+const gameEven = () => {
+  rulesOfTheGame(game, rule);
+};
 
 export default gameEven;
